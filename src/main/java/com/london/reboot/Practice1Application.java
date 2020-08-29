e2f3c51c654a1af8094805dc00e8f24e237ef1e0
Ilamparithi <ilamparithi.natarajan@wipro.com>
2020-08-29 20:11:14 +0100

    fix(swagger): use springfox starter
    this dep replaces all springfox deps
    
    also
    1. add tests for swagger endpoints
    2. add security configuration
    3. swagger config is not required in 3.0
    	only @EnableSwagger2 is required

--- src/main/java/com/london/reboot/Practice1Application.java
+++ src/main/java/com/london/reboot/Practice1Application.java
@@ -2,8 +2,15 @@ package com.london.reboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.spring.web.plugins.Docket;
 
 
+
+
+@EnableSwagger2
 @SpringBootApplication
 public class Practice1Application {
 
@@ -11,4 +18,8 @@ public class Practice1Application {
 		SpringApplication.run(Practice1Application.class, args);
 	}
 
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.OAS_30);
+	}
 }
