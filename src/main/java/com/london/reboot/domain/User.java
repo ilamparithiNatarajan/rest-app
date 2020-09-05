317995c632100dd53a38be70c1309e925602c764
Ilamparithi <ilamparithi.natarajan@wipro.com>
2020-09-05 15:00:00 +0100

    add javax validation

--- src/main/java/com/london/reboot/domain/User.java
+++ src/main/java/com/london/reboot/domain/User.java
@@ -2,7 +2,20 @@ package com.london.reboot.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public @Data class User {
+    @NotBlank(message = "firstName is mandatory")
+    @Size(min = 1, max = 25, message
+            = "About Me must be between 1 and 25 characters")
+    @Pattern(regexp = "[a-zA-Z]+", message = "must not contain number or special characters")
     private String firstName;
+
+    @NotBlank(message = "lastName is mandatory")
+    @Size(min = 1, max = 25, message
+            = "About Me must be between 1 and 25 characters")
+    @Pattern(regexp = "[a-zA-Z]+", message = "must not contain number or special characters")
     private String lastName;
 }
