6465c8bfbb7780f6c04455d0b8aa74e822a4c5ec
Ilamparithi <ilamparithi.natarajan@wipro.com>
2020-09-05 15:00:00 +0100

    add javax validation

--- src/main/java/com/london/reboot/controllers/UserController.java
+++ src/main/java/com/london/reboot/controllers/UserController.java
@@ -4,14 +4,18 @@ import com.london.reboot.domain.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 
-    @PostMapping
-    public String createUser(@RequestBody User user) {
+    @PostMapping(path = "/new", consumes = "application/json", produces = "text/plain")
+    public @ResponseBody
+    String createUser(@Valid @RequestBody User user) {
         String fullName = user.getFirstName() + " " + user.getLastName();
         return "user created : " + fullName;
     }
